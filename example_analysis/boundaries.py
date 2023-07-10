89c99aaf8551ed5ddf93a18ce7de03cfc53828ec
Humza Qureshi <hqureshi@primordial-piranha.hep.anl.gov>
2023-07-10 17:55:45 -0500

    this compiling stuff is killing me

--- example_analysis/boundaries.py
+++ example_analysis/boundaries.py
@@ -3,9 +3,14 @@ import matplotlib.pyplot as plt
 
 boundaries = []
 steps = []
+exits = []
+returns = []
+
+global_ranks = None
+global_on_boundary = None
 
 def onAnalysisStep(**kwargs):
-    global boundaries,steps
+    global boundaries,steps,global_ranks,global_on_boundary,exits,returns
     step = kwargs["step"]
     z = kwargs["z"]
     a = kwargs["a"]
@@ -13,19 +18,39 @@ def onAnalysisStep(**kwargs):
     ng = kwargs["ng"]
     rl = kwargs["rl"]
 
-    rank_centers = (particles[:,[0,1,2]]//(ng//2))*(ng//2)
+    ranks = (particles[:,[0,1,2]]//(ng//2))
+    rank_centers = ranks*(ng//2)
     rank_coords = particles[:,[0,1,2]] - rank_centers
     on_boundary = rank_coords > ((ng//2) - 1)
     on_boundary = np.logical_or(np.logical_or(on_boundary[:,0],on_boundary[:,1]),on_boundary[:,2])
+    ranks = ranks[:,0]*4 + ranks[:,1]*2 + ranks[:,2]
+    if step != 0:
+        exitted = np.logical_and(on_boundary,np.logical_not(global_on_boundary))
+        entered = np.logical_and(np.logical_not(on_boundary),global_on_boundary)
+        previous_rank = global_ranks[entered]
+        current_rank = ranks[entered]
+        returned = previous_rank == current_rank
+        exits.append(np.sum(exitted))
+        returns.append(np.sum(returned))
+
+    global_ranks = np.copy(ranks)
+    global_on_boundary = np.copy(on_boundary)
 
     boundaries.append(np.sum(on_boundary))
     steps.append(step)
 
+    with open("data.txt","w") as f:
+        f.write("step,exit,return\n")
+        for step,i,j in zip(steps[1:],exits,returns):
+            f.write(str(step)+","+str(i)+","+str(j)+"\n")
+
     if (step == 624):
-        plt.title("Particles on Boundary\nng=" + str(ng) + ", rl=" + str(rl))
-        plt.plot(steps,boundaries)
+    #    plt.title("Particles on Boundary\nng=" + str(ng) + ", rl=" + str(rl))
+        plt.plot(steps[1:],exits)
+        plt.plot(steps[1:],returns)
         plt.xlabel("step")
-        plt.ylabel("# of particles on boundary")
-        plt.tight_layout()
-        plt.savefig('on_boundary_ng' + str(ng) + "_rl" + str(int(rl)) + '.jpg')
+        plt.savefig("tmp.jpg")
+    #    plt.ylabel("# of particles on boundary")
+    #    plt.tight_layout()
+    #    plt.savefig('on_boundary_ng' + str(ng) + "_rl" + str(int(rl)) + '.jpg')
     
--- makefile
+++ makefile
@@ -9,6 +9,9 @@ HACCGPM_BUILD_DIR ?= build
 
 CUDA_DIR ?= /usr/local/cuda
 
+CUDA_ARCH_FLAGS ?= -arch=sm_60 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_86,code=sm_86 -gencode=arch=compute_87,code=sm_87 -gencode=arch=compute_86,code=compute_86
+
+
 main: driver.cpp $(HACCGPM_BUILD_DIR)/swfftmanager.o $(HACCGPM_BUILD_DIR)/transfers.o $(HACCGPM_BUILD_DIR)/timers.o $(HACCGPM_BUILD_DIR)/ccamb.o $(HACCGPM_BUILD_DIR)/cic.o $(HACCGPM_BUILD_DIR)/initializer.o $(HACCGPM_BUILD_DIR)/io.o $(HACCGPM_BUILD_DIR)/power.o $(HACCGPM_BUILD_DIR)/greens.o $(HACCGPM_BUILD_DIR)/solver.o $(HACCGPM_BUILD_DIR)/params.o $(HACCGPM_BUILD_DIR)/timestepper.o $(HACCGPM_BUILD_DIR)/mmanager.o $(HACCGPM_BUILD_DIR)/ffts.o $(HACCGPM_BUILD_DIR)/particleswapkernels.o | swfft 
 	mpicxx $^ $(SWFFT_DIR)/lib/swfft_a2a_gpu.a bdwgc/libgc.a -L$(CUDA_DIR)/lib64 -lcudart -lcufft $(PY_LD_FLAGS) -lpython3.9 -I$(CUDA_DIR)/include -fPIC -O3 -fopenmp -g -o haccgpm
 
@@ -29,7 +32,7 @@ $(HACCGPM_BUILD_DIR)/%.o: %.cpp | $(HACCGPM_BUILD_DIR)
 	mpicxx $< -I$(CUDA_DIR)/include -fPIC -O3 -fopenmp -g -c -o $@
 
 $(HACCGPM_BUILD_DIR)/%.o: %.cu | $(HACCGPM_BUILD_DIR)
-	nvcc $< -lcufft -lineinfo -Xptxas -v -Xcompiler -fPIC,-O3,-fopenmp,-g, -gencode arch=compute_60,code=sm_60 -c -o $@
+	nvcc $< -lcufft -lineinfo -Xptxas -v -Xcompiler -fPIC,-O3,-fopenmp,-g, $(CUDA_ARCH_FLAGS) -c -o $@
 
 .PHONY: clean
 clean:

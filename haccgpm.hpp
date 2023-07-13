b516cc0e020a08f36677485deaf2d254d6b05ffc
Humza Qureshi <hqureshi@roomba.com>
2023-07-13 17:12:24 -0500

    new

--- haccgpm.hpp
+++ haccgpm.hpp
@@ -40,7 +40,6 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 #define InvokeGPUKernelParallel(func,numBlocks,blockSize,...) ({if(world_rank == 0)printf("%s   Invoked %s\n",indent,TOSTRING(func));CPUTimer_t start = CPUTimer();func<<<numBlocks,blockSize>>>(__VA_ARGS__);gpuErrchk( cudaPeekAtLastError() );gpuErrchk( cudaDeviceSynchronize() );CPUTimer_t end = CPUTimer();CPUTimer_t t = end-start;if(world_rank == 0)printf("%s      %s took %llu us\n",indent,TOSTRING(func),t); t;})
 
-
 #define hostFFT_t double
 #define deviceFFT_t cufftDoubleComplex
 

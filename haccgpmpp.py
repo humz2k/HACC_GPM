b82f02e00b69c15402e97aae2645d798f1f91911
Humza Qureshi <hqureshi@primordial-piranha.hep.anl.gov>
2023-08-02 18:30:32 -0500

    s

--- haccgpmpp.py
+++ haccgpmpp.py
@@ -1,4 +1,29 @@
-from postprocessing import *
+#!/home/hqureshi/miniconda3/bin/python
 
-run = Run("runs/test")
-print(run.pks[0].folds[0])
+import postprocessing as pp
+import sys
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    pp.plot_pretty(dpi=300)
+
+    run = pp.Run("runs/test")
+
+    to_plot = [int(i) for i in sys.argv[2:]]
+
+    for i in to_plot:
+        pk = run.pks[i]
+
+        pk.plot(folds=[0],label="z=" + str(round(pk.z,1)) + ", fold=0",linewidth=0.9)
+        if (len(pk.folds) > 1):
+            pk.plot(folds=[1],label="z=" + str(round(pk.z,1)) + ", fold=1",linewidth=0.9)
+
+    run.plot_k_nyq(color="black",linestyles="dashed",linewidth=0.5)
+    run.plot_k_nyq_2(color="black",linestyles="dashed",linewidth=0.5)
+    plt.legend()
+
+    plt.title("Power Spectrum\n" + r"$N_g = " + str(run.params["NG"]) + r"$, $N_p = " + str(run.params["NP"]) + r"$, $L = " + str(run.params["RL"]) + r"$ Mpc",size=10)
+
+    plt.tight_layout()
+    plt.savefig(sys.argv[1])
--- postprocessing/run.py
+++ postprocessing/run.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 def z2a(z):
     return 1/(z + 1)
@@ -8,10 +10,23 @@ def a2z(a):
     return (1/a) - 1
 
 class PowerSpectrum:
-    def __init__(self,z,step,folds):
+    def __init__(self,z,step,folds,c):
         self.folds = folds
         self.step = step
         self.z = z
+        self.c = c
+    
+    def cmap(self,cm):
+        cmap = matplotlib.cm.get_cmap(cm)
+        return cmap(self.c)
+
+    def plot(self,folds=[0],**kwargs):
+        plt.xscale('log')
+        plt.yscale('log')
+        for fold in folds:
+            ks = self.folds[fold]["k"]
+            pks = self.folds[fold]["P_0(k)"]
+            plt.plot(ks,pks,**kwargs)
     
     def __str__(self):
         return "PowerSpectrum<z=" + str(self.z) + ",step=" + str(self.step) + ">"
@@ -21,20 +36,53 @@ class Run:
         self.base_name = base_name
         self.params = self.read_params()
         self.pks = self.find_pks()
+        self.k_nyq = (np.pi*self.params["NP"])/self.params["RL"]
+    
+    def plot_k_nyq(self,**kwargs):
+        ymin,ymax = plt.ylim()
+        plt.vlines([self.k_nyq],ymin,ymax,**kwargs)
+        plt.ylim(ymin,ymax)
+    
+    def plot_k_nyq_2(self,**kwargs):
+        ymin,ymax = plt.ylim()
+        plt.vlines([self.k_nyq/2],ymin,ymax,**kwargs)
+        plt.ylim(ymin,ymax)
+
+    def plot_k_nyq_4(self,**kwargs):
+        ymin,ymax = plt.ylim()
+        plt.vlines([self.k_nyq/2],ymin,ymax,**kwargs)
+        plt.ylim(ymin,ymax)
 
     def find_pks(self):
         a_init = z2a(self.params["Z_IN"])
         deltaT = (z2a(self.params["Z_FIN"]) - z2a(self.params["Z_IN"]))/self.params["N_STEPS"]
         out = []
+        try:
+            pks = [self.read_pk("ini",fold) for fold in range(self.params["PK_FOLDS"] + 1)]
+            pk = PowerSpectrum(float(self.params["Z_IN"]),"ini",pks,0.0)
+            out.append(pk)
+        except:
+            pass
         for i in range(self.params["N_STEPS"]):
             current_a = a_init + (deltaT * i)
             current_z = a2z(current_a)
             try:
                 pks = [self.read_pk(i,fold) for fold in range(self.params["PK_FOLDS"] + 1)]
-                pk = PowerSpectrum(current_z,i,pks)
+                pk = PowerSpectrum(current_z,i,pks,(i+1)/self.params["N_STEPS"])
                 out.append(pk)
             except:
                 pass
+        last_step = self.params["N_STEPS"]
+        if "LAST_STEP" in self.params:
+            last_step = self.params["LAST_STEP"]
+        current_a = a_init + (deltaT * last_step)
+        current_z = a2z(current_a)
+        try:
+            pks = [self.read_pk("fin",fold) for fold in range(self.params["PK_FOLDS"] + 1)]
+            pk = PowerSpectrum(current_z,"fin",pks,1.0)
+            out.append(pk)
+        except:
+            pass
         return out
     
     def read_params(self):
@@ -61,5 +109,20 @@ class Run:
         return out
     
 run = Run("/home/hqureshi/HACC_GPM/runs/test")
+
+def plot_pretty(dpi=150,fontsize=15):
+    plt.rcParams['figure.dpi']= dpi
+    plt.rc("savefig", dpi=dpi)
+    plt.rc('font', size=fontsize)
+    plt.rc('xtick', direction='in')
+    plt.rc('ytick', direction='in')
+    plt.rc('xtick.major', pad=5)
+    plt.rc('xtick.minor', pad=5)
+    plt.rc('ytick.major', pad=5)
+    plt.rc('ytick.minor', pad=5)
+    plt.rc('lines', dotted_pattern = [2., 2.])
+    plt.rc('legend',fontsize=5)
+    #plt.rc('text', usetex=True)
+    plt.rcParams['figure.figsize'] = [5, 4]
 
     
